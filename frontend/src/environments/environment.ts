0faae1f23deb5b1230e49902cc0629b0650a5a7a
Lucas Simão <lucas.simao@trt22.jus.br>
2019-05-09 16:52:10 -0300

    corrigindo parâmetros de conexao ao keycloak em ambiente de desenvolvimento

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   backend: "http://localhost:8080",
-  keycloak_backend: "http://keycloak.trt22.jus.br:8080/auth/realms/trt22/protocol/openid-connect/token",
-  client_id: "trt22-demo-backend"
+  keycloak_backend: "http://localhost:8081/auth/realms/master/protocol/openid-connect/token",
+  client_id: "demo-angular"
 };
 
 /*

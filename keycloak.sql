22c28e33b3163d601ca317a150c538b6fe940f57
Lucas Simão <lucas.simao@trt22.jus.br>
2019-05-09 14:40:07 -0300

    renomeando banco de dados

--- keycloak.sql
+++ keycloak.sql
@@ -1,8 +1,13 @@
 CREATE DATABASE
 IF NOT EXISTS keycloak_db;
 
+CREATE DATABASE
+IF NOT EXISTS crud_angular_spring_boot;
+
 CREATE USER 'keycloak'@'%' IDENTIFIED BY 'keycloak123';
 
 GRANT ALL PRIVILEGES ON keycloak_db.* TO 'keycloak'@'%'
 WITH
 GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON crud_angular_spring_boot.* TO 'app'@'%' WITH GRANT OPTION;
